7b16f8d0347e5c7f52e74276b4967db5156fcc1e
Examath <pauljis@outlook.com>
2023-08-16 11:57:06 +0930

    Test Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yFoil
 Generate NACA curve equations that are a function of x
+To be released by 10th of September, 2023
---
+++ cl.m
@@ -0,0 +1,3 @@
+% Command Line interface for yFoil
+fprintf("This is the command line interface for yFoil\n");
+query = input("Enter 4-digit NACA code:");
