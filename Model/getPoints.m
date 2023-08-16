7e0d9447a27ecb9255051837cc30d267f67115ea
Examath <pauljis@outlook.com>
2023-08-16 12:40:13 +0930

    v0.1 Testing finction access from separate file and stuff

---
+++ Model/getPoints.m
@@ -0,0 +1,5 @@
+function [outputArg1] = getPoints(chamber, chamberPos, thickness)
+%   Generate Points
+%   Detailed explanation goes here
+    outputArg1 = [1:10;10:-1:1];
+end
--- cl.m
+++ cl.m
@@ -1,3 +1,5 @@
 % Command Line interface for yFoil
 fprintf("This is the command line interface for yFoil\n");
 query = input("Enter 4-digit NACA code:");
+points = getPoints(1,2,3);
+plot(points);
