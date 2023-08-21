21f97e6ada1e4c04c6a918ba41a6d8007e8f6024
Examath <pauljis@outlook.com>
2023-08-21 20:59:16 +0930

    v0.2 NacaProfile: Actual NACA upper and lower surface

--- Model/NacaProfile.m
+++ Model/NacaProfile.m
@@ -1,49 +1,132 @@
 classdef NacaProfile
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+    % NacaProfile Represents a NACA profile
+    %   A naca profile is an aerofoil described by three parameters.
+    %   See https://en.wikipedia.org/wiki/NACA_airfoil
+
+    methods (Static)
+        function naca = GenerateFromDigits(inputDigits)
+            %GENERATEFROMDIGITS Trys to generate a naca profile from the 4-digit
+            %specifier
+            %   This attempts to create a NacaProfile instance from
+            %   the 4-digit specifier. success is true if t=it is succesful
+            arguments
+                % String containing the 4-digit specifier
+                inputDigits {mustBeTextScalar}
+            end
+
+            % Check if 4 digits
+            if (length(inputDigits) == 4)
+                maximumChamber = str2double(inputDigits(1)) / 100;
+                locationChamber = str2double(inputDigits(2)) / 10;
+                thickness = str2double(inputDigits(3:4)) / 100;
+
+                % Check if all valid
+                if (~(isnan(maximumChamber) || isnan(locationChamber) || isnan(thickness)))
+                    naca = NacaProfile(maximumChamber, locationChamber, thickness);
+                    return
+                end
+            end
+
+            naca = [];
+        end
+    end
 
     properties
-        % Lower and Upper surface stores the 2d points that make up the
-        % profile. They may not be the same size.
+        % Maximum chamber as a fraction of chord
+        M
+        % Position of the maximum chamber as a fraction of chord
+        P
+        % Thickness as a fraction of chord
+        T
+        % A 2 by m matrix that stores the points forming the upper surface 
+        % of the profile
         UpperSurface
+        % A 2 by m matrix that stores the points forming the lower surface 
+        % of the profile
         LowerSurface
     end
 
     methods
-        function this = NacaProfile(xPositions)
-            %NacaFoil Construct and generate an instance of the profile
-            %   This constructor generates a profle with the equation
-            %   y = +-x^2
+        function this = NacaProfile(maximumChamber, locationChamber, thickness)
+            % NacaProfile Construct and generate an instance of the profile
+            %   Note this does not generate the surface. 
+            %   Call ComputeSurface() to do that.
             arguments
-                % This, per matlab documentation, should ensure parameter
-                % is a vector and ensure
-                % it to a default of 0.1 intervals
-                xPositions {mustBeVector} = [0:0.1:1]
+                % All parameters should be scalar numbers
+                maximumChamber (1,1) {mustBeReal} = 0
+                locationChamber (1,1) {mustBeReal} = 0
+                thickness (1,1) {mustBeReal} = 0.3
             end
 
-            % Preallocate
-            this.UpperSurface = zeros(2,length(xPositions));
-            this.LowerSurface = zeros(2,length(xPositions));
+            % Set properties
+            this.M = maximumChamber;
+            this.P = locationChamber;
+            this.T = thickness;
+
+            % Compute surface
+            % TBC
+        end
+
+        function obj = ComputeSurface(obj, xPositions)
+            %COMPUTESURFACE Construct and generate an instance of the profile
+
+            % Preallocate matrix for efficiency
+            obj.UpperSurface = zeros(2,length(xPositions));
+            obj.LowerSurface = zeros(2,length(xPositions));
 
             % To generate surface, loop through the provided x-positions
             % and generate respective point on both surfaces:
             for index = 1:length(xPositions)
-                % In the real NACA curve, the x pos is complexly defined
-                % But for now just assign
-                this.UpperSurface(1, index) = xPositions(index);
-                this.LowerSurface(1, index) = xPositions(index);
-                
-                % Creates a quadratic surface
-                this.UpperSurface(2, index) = xPositions(index) ^ 2;
-                this.LowerSurface(2, index) = - xPositions(index) ^ 2;
+                % Get point pair (it is more efficient to simultaneously
+                % compute corresponding upper and lower points
+                pointPair = ComputePointPair(obj, xPositions(index));
+                obj.UpperSurface(:, index) = pointPair(:,1);
+                obj.LowerSurface(:, index) = pointPair(:,2);
             end
         end
 
-        % Part of template, not needed for now
-        % function outputArg = method1(obj,inputArg)
-        %     %METHOD1 Summary of this method goes here
-        %     %   Detailed explanation goes here
-        %     outputArg = obj.Property1 + inputArg;
-        % end
+        function pointPair = ComputePointPair(obj, x)
+            %COMPUTEPOINTPAIR Gets a pair of points for a position on the
+            % airfoil. This returns a 2x2 matrix, with the first column
+            % being the upper surface point and the second column being the
+            % lower surface point.
+
+            % Using formulas on https://en.wikipedia.org/wiki/NACA_airfoil
+
+            % Compute thickness
+            yt = obj.T * 5 * (0.2969 * sqrt(x) - 0.1260 * x - 0.3516 * x ^ 2 + 0.2843 * x ^ 3 - 0.1015 * x ^ 4);
+
+            % Check if the maximumChamber is non-zero
+            if (obj.M > 0)
+
+                % Compute chamber line
+                if (x <= obj.P)
+                    yc = obj.M * (2 * obj.P * x - x ^ 2) / obj.P ^ 2;
+                else
+                    yc = obj.M * ((1 - 2 * obj.P) + 2 * obj.P * x - x ^ 2) / (1 - obj.P) ^ 2;
+                end
+
+                % Compute derivative of chamber line
+                if (x <= obj.P)
+                    dycdx = 2 * obj.M * (obj.P - x) / obj.P ^ 2;
+                else
+                    dycdx = 2 * obj.M * (obj.P - x) / (1 - obj.P) ^ 2;
+                end
+
+                % Compute normal angle of chamber line
+                theta = atan(dycdx);
+
+                % Get x and y components of thickness 
+                % (as thickness is normal to chamber)
+                ytcost = yt * cos(theta);
+                ytsint = yt * sin(theta);
+
+                % Return upper and lower point
+                pointPair = [x - ytsint, x + ytsint; yc + ytcost, yc - ytcost];
+
+            else % If the chamber is zero, then use a simplified process
+                pointPair = [x, x; yt, -yt];
+            end
+        end
     end
 end
--- cl.m
+++ cl.m
@@ -2,27 +2,48 @@
 
 %% Input
 fprintf("This is the command line interface for yFoil\n");
-%query = input("Enter 4-digit NACA code:"); % No input for now
+
+% Ask for the four digit naca specifier 
+% (see https://en.wikipedia.org/wiki/NACA_airfoil)
+query = input("Enter 4-digit NACA code: ", "s");
 
 %% Generate NACA Profile
-% Init default NACA
-naca = NacaProfile;
+% Create an instance of a NacaProfile with the specified query
+naca = NacaProfile.GenerateFromDigits(query);
 
-% Plot the upper surface and lower surface on same figure
-close all;
-hold on;
-plot(naca.UpperSurface(1,:), naca.UpperSurface(2,:), "Color","blue");
-plot(naca.LowerSurface(1,:), naca.LowerSurface(2,:), "Color", "red");
-hold off;
+% Create a vector with the independent variable for the naca profile. 
+% This creates a profile with 20 points. The .^ ensures the points are
+% concentrated towards the left leading edge of the profile.
+xPointVector = (0:0.05:1).^2;
+
+% If an error occures in NacaProfile.GenerateFromDigits(), then the method
+% returns empty. So check if it is empty, print error message, and stop
+% execution.
+if (isempty(naca))
+    fprintf('Invalid NACA specifier. See https://en.wikipedia.org/wiki/NACA_airfoil\n');
+    return
+end
+
+% Compute the surface of the profile with the specified xPositionVector
+% resolution.
+naca = ComputeSurface(naca, xPointVector);
 
 %% Generate yFoil
+% TBC
 
 %% Output Preview
 
-%% Output
+close all;
+hold on;
 
-% For now, fprintf the y of the upper, lower surfaces
-fprintf('%.2f ', naca.UpperSurface(2,:));
-fprintf('\n');
-fprintf('%.2f ', naca.LowerSurface(2,:));
-fprintf('\n');
+% Ensure that the plot scale is square (so that there's no distortion)
+daspect([1 1 1])
+
+% Plot the upper surface and lower surface on same figure
+plot(naca.UpperSurface(1,:), naca.UpperSurface(2,:), "Color","blue");
+plot(naca.LowerSurface(1,:), naca.LowerSurface(2,:), "Color", "red");
+
+hold off;
+
+%% Output
+% TBC
